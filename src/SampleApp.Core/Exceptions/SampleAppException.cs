4126aa91e62cc196baf73e6091ed7030e66083bf
arashrohani <arash.rohani@gmail.com>
2017-01-20 18:54:45 -0500

    Adding Exception class

---
+++ src/SampleApp.Core/Exceptions/SampleAppException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SampleApp.Core.Exceptions
+{
+    public class SampleAppException : Exception
+    {
+        public SampleAppException()
+        {
+        }
+
+        public SampleAppException(string message) : base(message)
+        {
+        }
+
+        public SampleAppException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
