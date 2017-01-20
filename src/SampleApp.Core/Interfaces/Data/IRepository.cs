e3bca6691e8562b4e5ac31ba6daae9f2e189508b
arashrohani <arash.rohani@gmail.com>
2017-01-19 21:16:56 -0500

    Adding repository files to interfaces folder

--- src/SampleApp.Core/Interfaces/Data/IRepository.cs
+++ src/SampleApp.Core/Interfaces/Data/IRepository.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace SampleApp.Repository
+namespace SampleApp.Core.Interfaces.Data
 {
     public interface IRepository
     {
--- src/SampleApp.Repository/RepositoryBase.cs
+++ src/SampleApp.Repository/RepositoryBase.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using SampleApp.Core.Entities;
+using SampleApp.Core.Interfaces.Data;
 
 namespace SampleApp.Repository
 {
