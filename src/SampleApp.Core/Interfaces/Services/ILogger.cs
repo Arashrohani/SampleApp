ccab92ab2dc77e53f510da747957ef7db0e561b8
arashrohani <arash.rohani@gmail.com>
2017-01-23 18:31:50 -0500

    Removing LoggerService

--- src/SampleApp.Core/Interfaces/Services/ILogger.cs
+++ src/SampleApp.Core/Interfaces/Services/ILogger.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+
 namespace SampleApp.Core.Interfaces.Services
 {
     public interface ILogger
     {
+        
     }
 }
